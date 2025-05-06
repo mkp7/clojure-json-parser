f7f4bd5c46e5e46a15ab716356082df6b9d0f5aa
Mehulkumar Parmar <m3hul.g4m3r@gmail.com>
2025-05-07 02:23:22 +0530

    implement basic string parser
    Signed-off-by: Mehulkumar Parmar <m3hul.g4m3r@gmail.com>

--- src/clojure_json_parser/core.clj
+++ src/clojure_json_parser/core.clj
@@ -16,5 +16,18 @@
         (let [number-match (get number-matcher 0)]
           [(Double/parseDouble number-match) (subs input (.length number-match))]))))
 
+(defn parse-string-chars [input]
+  (if (= (.length input) 0) nil
+      (if (str/starts-with? input "\"") ["" input]
+          (let [char-matcher (parse-string-chars (subs input 1))]
+            (if (nil? char-matcher) nil
+                [(str (subs input 0 1) (get char-matcher 0)) (get char-matcher 1)])))))
+
+(defn parse-string [input]
+  (if (not (str/starts-with? input "\"")) nil
+      (let [string-chars-matcher (parse-string-chars (subs input 1))]
+        (if (nil? string-chars-matcher) nil
+            [(get string-chars-matcher 0) (subs (get string-chars-matcher 1) 1)]))))
+
 "tests"
 (json/read-str "null")
