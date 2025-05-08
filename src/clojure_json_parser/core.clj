fbf9af0c7ce822247ae307f9cade204baaa5900c
Mehulkumar Parmar <m3hul.g4m3r@gmail.com>
2025-05-08 11:20:33 +0530

    implement tests for json value parser
    Signed-off-by: Mehulkumar Parmar <m3hul.g4m3r@gmail.com>

--- src/clojure_json_parser/core.clj
+++ src/clojure_json_parser/core.clj
@@ -1,7 +1,5 @@
 (ns clojure-json-parser.core
-  (:require
-   [clojure.string :as str]
-   [clojure.data.json :as json]))
+  (:require [clojure.string :as str]))
 
 (defn parse-null [input]
   (if (str/starts-with? input "null") [nil (subs input 4)] nil))
@@ -11,7 +9,7 @@
       (if (str/starts-with? input "false") [false (subs input 5)] nil)))
 
 (defn parse-number [input]
-  (let [number-matcher (re-find #"^-?[1-9]\d*(\.\d+)?((e|E)(\+|-)?\d+)?" input)]
+  (let [number-matcher (re-find #"^-?(0|([1-9]\d*))(\.\d+)?((e|E)(\+|-)?\d+)?" input)]
     (if (nil? number-matcher) nil
         (let [number-match (get number-matcher 0)]
           [(Double/parseDouble number-match) (subs input (.length number-match))]))))
@@ -22,24 +20,17 @@
     [(get str-esc-chars (subs input 0 1)) (subs input 1)]
     nil))
 
-(parse-esc-char "nn")
-
 (defn parse-unicode-char [input]
   (let [unicode-char-matcher (re-find #"^u([a-fA-F0-9]{4})" input)]
     (if (nil? unicode-char-matcher) nil
         [(char (Integer/parseInt (get unicode-char-matcher 1) 16))
          (subs input 5)])))
 
-(def control-chars #{1 0 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 127})
+(def control-chars #{1 0 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 127})
 (defn parse-string-char [input]
   (if (= (.length input) 0) nil
       (if (get control-chars (int (get input 0))) nil
           [(subs input 0 1) (subs input 1)])))
-
-(some #(str/starts-with? "\n" (str (char %))) control-chars)
-(boolean (get control-chars 0))
-(int (get "a\b" 0))
-(parse-string-char "")
 
 (defn parse-string-chars [input]
   (if (= (.length input) 0) nil
@@ -65,6 +56,7 @@
         (if (nil? string-chars-matcher) nil
             [(get string-chars-matcher 0) (subs (get string-chars-matcher 1) 1)]))))
 
+
 (defn parse-array-values [input]
   (if (not (str/starts-with? (str/triml input) ",")) [[] input]
       (let [value-matcher (parse-value (subs (str/triml input) 1))]
@@ -72,8 +64,6 @@
             (let [parse-array-values-match (parse-array-values (get value-matcher 1))]
               (if (nil? parse-array-values-match) nil
                   [(into [(get value-matcher 0)] (get parse-array-values-match 0)) (get parse-array-values-match 1)]))))))
-(parse-array-values "\r , \n 1 \t,null   , \"abc\"   blabla")
-(parse-array-values ",null,1,\"\"  abc")
 
 (defn parse-array [input]
   (if (not (str/starts-with? input "[")) nil
@@ -84,9 +74,6 @@
                   (let [array-values (parse-array-values (str/triml (get value-matcher 1)))]
                     (if (or (nil? array-values) (not (str/starts-with? (str/triml (get array-values 1)) "]"))) nil
                         [(into [(get value-matcher 0)] (get array-values 0)) (subs (str/triml (get array-values 1)) 1)]))))))))
-(parse-array "[ 1, [ 123.9 ] \t\n ]  s")
-(parse-array "[]")
-(json/read-str "   [ 1, [ 123.9 ]\t\n ]  ")
 
 (defn parse-object-key-value [input]
   (let [object-key-matcher (parse-string (str/triml input))]
@@ -96,8 +83,6 @@
               (let [object-value-matcher (parse-value (subs (str/triml input) 1))]
                 (if (nil? object-value-matcher) nil
                     [{object-key (get object-value-matcher 0)} (get object-value-matcher 1)])))))))
-(parse-object-key-value " \r \"a\" \n : \t1bla")
-(parse-object-key-value " \r  \n : \t1bla")
 
 (defn parse-object-key-values [input]
   (if (not (str/starts-with? (str/triml input) ",")) [{} input]
@@ -106,13 +91,6 @@
             (let [parse-object-key-values-match (parse-object-key-values (get key-value-match 1))]
               (if (nil? parse-object-key-values-match) nil
                   [(into (get key-value-match 0) (get parse-object-key-values-match 0)) (get parse-object-key-values-match 1)]))))))
-(parse-object-key-values "   ,  \"a\"  :   1   ,   \"b\"  :  2     \"c\"  :  3  ")
-(parse-object-key-values "     \"a\"  :   1   ,   \"b\"  :  2     \"c\"  :  3  ")
-(parse-object-key-values "   ,   :   1   ,   \"b\"  :  2     \"c\"  :  3  ")
-(parse-object-key-values ", \"a\": 2}")
-
-(let [input "   ,   :   1   ,   \"b\"  :  2     \"c\"  :  3  "]
-  (if (not (str/starts-with? (str/triml input) ",")) [{} input] nil))
 
 (defn parse-object [input]
   (if (not (str/starts-with? input "{")) nil
@@ -124,38 +102,9 @@
                     (if (or (nil? object-key-values) (not (str/starts-with? (str/triml (get object-key-values 1)) "}"))) nil
                         [(into (get object-key-value 0) (get object-key-values 0)) (subs (str/triml (get object-key-values 1)) 1)]))))))))
 
-(parse-object "{}")
-(parse-object "{\"b\": 1, \"a\": 2}")
-(json/read-str "{}")
-(json/read-str "{\"b\": 1, \"a\": 2}")
-
 (defn parse-value [input] (some #(% (str/triml input)) [parse-null parse-bool parse-number parse-string parse-array parse-object]))
-(parse-value "123abc")
 
 (defn parse-json [input]
   (let [value-match (parse-value input)]
     (if (or (nil? value-match) (> (.length (str/triml (get value-match 1))) 0)) nil
         (get value-match 0))))
-(json/read-str "{\"b\": 1, \"a\": [2]}")
-(parse-json "{\"b\": 1, \"a\": [2]}")
-
-;; Macros
-(defmacro backwards [form] (reverse form))
-(backwards (3 2 1 +))
-(backwards (\c \b \a str))
-(defmacro infix [[arg1 fnc arg2]] (list fnc arg1 arg2))
-(infix (1 + 2))
-(macroexpand '(infix (1 + 2)))
-(defn sum-two [a b] (infix (a + b)))
-(sum-two 1 2)
-
-"check falsy or truthy values"
-(boolean nil)
-
-"parse hex char"
-(parse-unicode-char "uabcde")
-
-"parse string"
-(def json-string "\"\\uabcde\\u8347\\n\"kadksjhda")
-(parse-string json-string)
-(json/read-str json-string)
---
+++ src/clojure_json_parser/scrap.clj
@@ -0,0 +1,69 @@
+(ns clojure-json-parser.scrap
+  (:require
+   [clojure-json-parser.core :refer [control-chars parse-array
+                                     parse-array-values parse-esc-char
+                                     parse-json parse-object
+                                     parse-object-key-value
+                                     parse-object-key-values parse-string
+                                     parse-string-char parse-unicode-char
+                                     parse-value]]
+   [clojure.data.json :as json]
+   [clojure.string :as str]
+   [clojure.java.io :as io]))
+
+
+(parse-esc-char "nn")
+(some #(str/starts-with? "\n" (str (char %))) control-chars)
+(boolean (get control-chars 0))
+(int (get "a\b" 0))
+(parse-string-char "")
+(parse-string "\"JSON Test\"")
+(parse-array-values "\r , \n 1 \t,null   , \"abc\"   blabla")
+(parse-array-values ",null,1,\"\"  abc")
+(parse-array "[ 1, [ 123.9 ] \t\n ]  s")
+(parse-array "[]")
+(json/read-str "   [ 1, [ 123.9 ]\t\n ]  ")
+(parse-object-key-value " \r \"a\" \n : \t1bla")
+(parse-object-key-value " \r  \n : \t1bla")
+(parse-object-key-values "   ,  \"a\"  :   1   ,   \"b\"  :  2     \"c\"  :  3  ")
+(parse-object-key-values "     \"a\"  :   1   ,   \"b\"  :  2     \"c\"  :  3  ")
+(parse-object-key-values "   ,   :   1   ,   \"b\"  :  2     \"c\"  :  3  ")
+(parse-object-key-values ", \"a\": 2}")
+(parse-object "{}")
+(parse-object "{\"b\": 1, \"a\": 2}")
+(json/read-str "{}")
+(json/read-str "{\"b\": 1, \"a\": 2}")
+(parse-value "123abc")
+(json/read-str "{\"b\": 1, \"a\": [2]}")
+(parse-json "{\"b\": 1, \"a\": [2]}")
+(let [input "   , \"a\"  :   1   ,   \"b\"  :  2     \"c\"  :  3  "]
+  (if (not (str/starts-with? (str/triml input) ",")) [{} input] nil))
+
+
+;; Macros
+(defmacro backwards [form] (reverse form))
+(backwards (3 2 1 +))
+(backwards (\c \b \a str))
+(defmacro infix [[arg1 fnc arg2]] (list fnc arg1 arg2))
+(infix (1 + 2))
+(macroexpand '(infix (1 + 2)))
+(defn sum-two [a b] (infix (a + b)))
+(sum-two 1 2)
+
+"check falsy or truthy values"
+(boolean nil)
+
+"parse hex char"
+(parse-unicode-char "uabcde")
+
+"parse string"
+(def json-string "\"\\uabcde\\u8347\\n\"kadksjhda")
+(parse-string json-string)
+(json/read-str json-string)
+
+(let [test-files
+      (map #(.getAbsolutePath %)
+           (filter #(.isFile %) (file-seq (io/file "./test/clojure_json_parser/cases"))))]
+  (map #(list % (not (nil? (parse-json (slurp %)))))
+       test-files))
+;; (re-find #"^pass.+" (.getName %))
