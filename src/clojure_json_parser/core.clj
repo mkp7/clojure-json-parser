2c0790e2766c0525df537bfa54dca62f16d4132b
Mehulkumar Parmar <m3hul.g4m3r@gmail.com>
2025-05-15 06:56:13 +0530

    create and refactor using --> macro
    Signed-off-by: Mehulkumar Parmar <m3hul.g4m3r@gmail.com>

--- src/clojure_json_parser/core.clj
+++ src/clojure_json_parser/core.clj
@@ -4,6 +4,9 @@
 
 (declare parse-null parse-bool parse-number parse-string parse-array parse-object parse-value)
 
+(defmacro --> [& form]
+  (conj (reverse form) '->>))
+
 (defn parse-null [input]
   (when (str/starts-with? input "null")
     [nil (subs input 4)]))
@@ -14,20 +17,19 @@
     (str/starts-with? input "false") [false (subs input 5)]))
 
 (defn parse-number [input]
-  (->>
-    [(Double/parseDouble number-match)
-     (subs input (.length number-match))]
-    (let [number-match (get number-matcher 0)])
+  (-->
+    (let [number-matcher (re-find #"^-?(0|([1-9]\d*))(\.\d+)?((e|E)(\+|-)?\d+)?" input)])
     (when number-matcher)
-    (let [number-matcher (re-find #"^-?(0|([1-9]\d*))(\.\d+)?((e|E)(\+|-)?\d+)?" input)])))
+    (let [number-match (get number-matcher 0)])
+    [(Double/parseDouble number-match)
+     (subs input (.length number-match))]))
 
 (def str-esc-chars {"\"" "\"", "\\" "\\", "/" "/", "b" "\b", "f" "\f", "n" "\n", "r" "\r", "t" "\t"})
 (defn parse-esc-char [input]
   (when (->>
           (subs input 0 1)
           (get str-esc-chars)
-          (and
-            (> (.length input) 0)))
+          (and (> (.length input) 0)))
     [(get str-esc-chars (subs input 0 1))
      (subs input 1)]))
 
@@ -52,40 +54,41 @@
      (subs input 1)]))
 
 (defn parse-string-chars [input]
-  (->>
+  (-->
+    (loop [data "" input input])
+    (when (> (.length input) 0))
     (cond
       (str/starts-with? input "\"") [data input]
-      (str/starts-with? input "\\") (->>
-                                      (recur (str data (get esc-char-matcher 0))
-                                        (get esc-char-matcher 1))
+      (str/starts-with? input "\\") (-->
+                                      (let [esc-char-matcher (->>
+                                                               (subs input 1)
+                                                               (parse-esc-char)
+                                                               (or (parse-unicode-char (subs input 1))))])
                                       (when esc-char-matcher)
-                                      (let [esc-char-matcher (or (parse-unicode-char (subs input 1))
-                                                               (parse-esc-char (subs input 1)))]))
-      :else (->>
-              (recur (str data (get string-char-matcher 0))
-                (get string-char-matcher 1))
+                                      (recur (str data (get esc-char-matcher 0))
+                                        (get esc-char-matcher 1)))
+      :else (-->
+              (let [string-char-matcher (parse-string-char input)])
               (when string-char-matcher)
-              (let [string-char-matcher (parse-string-char input)])))
-    (when (> (.length input) 0))
-    (loop [data "" input input])))
+              (recur (str data (get string-char-matcher 0))
+                (get string-char-matcher 1))))))
 
 (defn parse-string [input]
-  (->>
-    [(get string-chars-matcher 0)
-     (subs (get string-chars-matcher 1) 1)]
-    (when string-chars-matcher)
+  (-->
+    (when (str/starts-with? input "\""))
     (let [string-chars-matcher (parse-string-chars (subs input 1))])
-    (when (str/starts-with? input "\""))))
+    (when string-chars-matcher)
+    [(get string-chars-matcher 0)
+     (subs (get string-chars-matcher 1) 1)]))
 
 (defn parse-array-values [input]
-  (->>
-    (recur (into data [(get value-matcher 0)])
-      (get value-matcher 1))
-    (when value-matcher)
+  (-->
+    (loop [data [] input input])
+    (if (not (str/starts-with? (str/triml input) ",")) [data input])
     (let [value-matcher (parse-value (subs (str/triml input) 1))])
-    (if (not (str/starts-with? (str/triml input) ","))
-      [data input])
-    (loop [data [] input input])))
+    (when value-matcher)
+    (recur (into data [(get value-matcher 0)])
+      (get value-matcher 1))))
 
 (defn parse-array [input]
   (cond
@@ -97,72 +100,82 @@
                                     (subs input 1)
                                     (str/triml)
                                     (subs 1))]
-    :else (->>
-            [(into [(get value-matcher 0)] (get array-values 0))
-             (->
-               (get array-values 1)
-               (str/triml)
-               (subs 1))]
-            (when (and array-values (->
-                                      (get array-values 1)
-                                      (str/triml)
-                                      (str/starts-with? "]"))))
+    :else (-->
+            (let [value-matcher (->>
+                                  (subs input 1)
+                                  (str/triml)
+                                  (parse-value))])
+            (when value-matcher)
             (let [array-values (->>
                                  (get value-matcher 1)
                                  (str/triml)
                                  (parse-array-values))])
-            (when value-matcher)
-            (let [value-matcher (->>
-                                  (subs input 1)
-                                  (str/triml)
-                                  (parse-value))]))))
+            (when (and array-values
+                    (->
+                      (get array-values 1)
+                      (str/triml)
+                      (str/starts-with? "]"))))
+            [(into [(get value-matcher 0)] (get array-values 0))
+             (->
+               (get array-values 1)
+               (str/triml)
+               (subs 1))])))
 
 (defn parse-object-key-value [input]
-  (->>
-    [{object-key (get object-value-matcher 0)}
-     (get object-value-matcher 1)]
-    (when object-value-matcher)
-    (let [object-value-matcher (parse-value (subs (str/triml input) 1))])
-    (when (str/starts-with? (str/triml input) ":"))
-    (let [object-key (get object-key-matcher 0) input (get object-key-matcher 1)])
+  (-->
+    (let [object-key-matcher (parse-string (str/triml input))])
     (when object-key-matcher)
-    (let [object-key-matcher (parse-string (str/triml input))])))
+    (let [object-key (get object-key-matcher 0)
+          input (get object-key-matcher 1)])
+    (when (str/starts-with? (str/triml input) ":"))
+    (let [object-value-matcher (parse-value (subs (str/triml input) 1))])
+    (when object-value-matcher)
+    [{object-key (get object-value-matcher 0)}
+     (get object-value-matcher 1)]))
 
 (defn parse-object-key-values [input]
-  (->>
-    (recur (into data (get key-value-match 0))
-      (get key-value-match 1))
-    (when key-value-match)
+  (-->
+    (loop [data {} input input])
+    (if (not (str/starts-with? (str/triml input) ",")) [data input])
     (let [key-value-match (parse-object-key-value (subs (str/triml input) 1))])
-    (if (not (str/starts-with? (str/triml input) ","))
-      [data input])
-    (loop [data {} input input])))
+    (when key-value-match)
+    (recur (into data (get key-value-match 0))
+      (get key-value-match 1))))
 
 (defn parse-object [input]
   (cond
     (not (str/starts-with? input "{")) nil
     (str/starts-with? (str/triml (subs input 1)) "}") [{} (subs (str/triml (subs input 1)) 1)]
-    :else (->>
-            [(into (get object-key-value 0) (get object-key-values 0))
-             (subs (str/triml (get object-key-values 1)) 1)]
-            (when (and object-key-values (str/starts-with? (str/triml (get object-key-values 1)) "}")))
-            (let [object-key-values (parse-object-key-values (str/triml (get object-key-value 1)))])
+    :else (-->
+            (let [object-key-value (parse-object-key-value (str/triml (subs input 1)))])
             (when object-key-value)
-            (let [object-key-value (parse-object-key-value (str/triml (subs input 1)))]))))
+            (let [object-key-values (parse-object-key-values (str/triml (get object-key-value 1)))])
+            (when (and object-key-values
+                    (str/starts-with? (str/triml (get object-key-values 1)) "}")))
+            [(into (get object-key-value 0) (get object-key-values 0))
+             (subs (str/triml (get object-key-values 1)) 1)])))
 
 (def value-parsers [parse-null parse-bool parse-number parse-string parse-array parse-object])
 (defn parse-value [input] (some #(% (str/triml input)) value-parsers))
 
 (defn parse-json [input]
   (let [value-match (parse-value input)]
-    (when (and value-match (= (str/triml (get value-match 1)) ""))
+    (when (and value-match
+            (= (str/triml (get value-match 1)) ""))
       (get value-match 0))))
 
 (defn test-json-parser []
   (let [test-files
-        (map #(.getAbsolutePath %)
-          (filter #(.isFile %) (file-seq (io/file "./test/clojure_json_parser/cases"))))]
-    (map #(list % (not (nil? (parse-json (slurp %)))))
+        (->>
+          (io/file "./test/clojure_json_parser/cases")
+          (file-seq)
+          (filter #(.isFile %))
+          (map #(.getAbsolutePath %)))]
+    (map #(list % (->>
+                    (slurp %)
+                    (parse-json)
+                    (nil?)
+                    (not)))
       test-files)))
 
 (parse-json (slurp "./test/clojure_json_parser/cases/pass1.json"))
